74e3fdf30f6348c8f68635e9eaee5bb9d1dbff47
Berkmann18 <maxieberkmann@gmail.com>
2019-03-06 18:06:32 +0000

    New: Palindrome Numbers

---
+++ PalindromeNum/index.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number} x
+ * @return {boolean}
+ */
+var isPalindrome = function(x) {
+  if (x < 0) return false;
+  let s = '' + x,
+    len = s.length / 2 | 0;
+  for (let i = 0; i < len; ++i) {
+    if (s[i] !== s[s.length - 1 - i]) return false;
+  }
+  return true
+};
+
+module.exports = isPalindrome
---
+++ PalindromeNum/index.test.js
@@ -0,0 +1,6 @@
+const t = require('../t')(require('./'));
+
+test('ex.1', () => t(121, true));
+test('ex.2', () => t(-121, false));
+test('ex.3', () => t(10, false));
+test('ex.4', () => t(11, true));
