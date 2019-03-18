aa98fda5efdfe821a1a33279232f4321760f8074
Berkmann18 <maxieberkmann@gmail.com>
2019-03-18 12:40:44 +0000

    New: KthLargest challenge (JS/Python)

---
+++ KthLargest/index.js
@@ -0,0 +1,8 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+const findKthLargest = (nums, k) => nums.sort((a, b) => b - a)[k - 1];
+
+module.exports = findKthLargest;
