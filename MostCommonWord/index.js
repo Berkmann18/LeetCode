c0cf0c42979e03bb7d34f09340829388547dc949
Berkmann18 <maxieberkmann@gmail.com>
2019-07-10 11:55:27 +0100

    Chore: Reformatting

--- MostCommonWord/index.js
+++ MostCommonWord/index.js
@@ -1,6 +1,6 @@
 'use strict';
-Object.defineProperty(exports, '__esModule', { value: true });
-const tokenizer = (data) => {
+Object.defineProperty(exports, '__esModule', {value: true});
+const tokenizer = data => {
   return data
     .split(/[!?',; .]+/g)
     .filter(Boolean)
@@ -10,23 +10,19 @@ const mostCommonWord = (paragraph, banned) => {
   const features = {};
   let mcw = '';
   let featMax = 0;
-  paragraph
-    .split(/[!?',; .]+/g)
-    .forEach(token => {
-      if (token.length) {
-        const tk = token.toLowerCase();
-        if (!banned.includes(tk)) {
-          if (tk in features)
-            ++features[tk];
-          else
-            features[tk] = 1;
-          if (features[tk] > featMax) {
-            featMax = features[tk];
-            mcw = tk;
-          }
+  paragraph.split(/[!?',; .]+/g).forEach(token => {
+    if (token.length) {
+      const tk = token.toLowerCase();
+      if (!banned.includes(tk)) {
+        if (tk in features) ++features[tk];
+        else features[tk] = 1;
+        if (features[tk] > featMax) {
+          featMax = features[tk];
+          mcw = tk;
         }
       }
-    });
+    }
+  });
   return mcw;
 };
 exports.default = mostCommonWord;
